8bcd2969174e4c4ee44b818608d786393dcf28a4
Khushi Singh <khushi.singh092001@gmail.com>
2024-09-20 12:43:34 +0530

    updated all requirements

--- src/components/gallerySection.jsx
+++ src/components/gallerySection.jsx
@@ -22,12 +22,13 @@ const GallerySection = () => {
   const displayedImages = isExpanded ? allImages : allImages.slice(0, 4);
 
   return (
+    <>
     <div className="bg-white py-12">
       <div className="max-w-6xl mx-auto px-8">
         <h2 className="text-2xl font-bold mb-4">
           <span className="border-l-4 border-yellow-400 pl-2">Gallery</span>
         </h2>
-        <p className="text-lg text-gray-600 mb-8">All Photos & Video</p>
+        <p className="text-lg text-gray-600 mb-8">All Photos & Videos</p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
           {displayedImages.map((src, index) => (
             <img
@@ -57,7 +58,46 @@ const GallerySection = () => {
         </div>
       </div>
     </div>
+
+<div className="bg-white py-12">
+      <div className="max-w-6xl mx-auto px-8">
+        {/* <h2 className="text-2xl font-bold mb-4">
+          <span className="border-l-4 border-yellow-400 pl-2">Gallery</span>
+        </h2> */}
+        <p className="text-lg text-gray-600 mb-8">Amenities</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+          {displayedImages.map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`Gallery item ${index + 1}`}
+              className="w-full h-48 object-cover rounded-lg"
+            />
+          ))}
+        </div>
+        <div className="text-center">
+          {!isExpanded ? (
+            <button
+              className="bg-yellow-400 text-black py-2 px-4 rounded-full text-lg font-bold"
+              onClick={() => setIsExpanded(true)}
+            >
+              View All
+            </button>
+          ) : (
+            <button
+              className="bg-yellow-400 text-black py-2 px-4 rounded-full text-lg font-bold"
+              onClick={() => setIsExpanded(false)}
+            >
+              Back
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+</>
   );
 };
+
+
 
 export default GallerySection;
